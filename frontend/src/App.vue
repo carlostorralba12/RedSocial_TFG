719e3e27c683acc8ade3274b17f1effc43439fc0
Carlos Torralba Ruiz <carlostorralba12@gmail.com>
2021-02-07 15:55:07 +0100

    Añado prototipo de footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@
       color="primary"
       dark
     >
+      <!--BUTON HOME-->
       <router-link class="d-flex align-center" to="/">
         <v-img
           alt="Vuetify Logo"
@@ -25,23 +26,49 @@
         color="primary"
         depressed
         elevation="13"
-        to="/register"
+        to="register"
       >Registro</v-btn>
+
+       <v-btn
+        color="primary"
+        depressed
+        elevation="13"
+        to="login"
+      >Login</v-btn>
     </v-app-bar>
+
     <!--MAIN COMPONENTS CONTAINER-->
     <v-main>
+
       <router-view>
       </router-view>
+
     </v-main>
+    
+    <div class="footer-app">
+
+      <Footer/>
+
+    </div>
+
   </v-app>
+
 </template>
 
+<style scoped lang="scss">
+  @import './styles/global.scss';
+</style>
+
 <script>
+import Footer from './components/Footer/Footer'
 
 export default {
   name: 'App',
   data: () => ({
-    //
-  })
+     
+  }),
+  components: {
+    Footer
+  }
 }
 </script>
