0b89e9e57d09ac10f3baa184a16e39a80448b278
carlostorralba12 <carlostorralba12@gmail.com>
2021-04-24 12:17:22 +0200

    Backend: Seguimiento de usuarios y comunidades realizado

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -17,5 +17,10 @@ api.get('/users/:id', mdAuth.ensureAuth, UserController.getUser);
 api.put('/users/:id', mdAuth.ensureAuth, UserController.updateUser);
 api.delete('/users/:id', mdAuth.ensureAuth, UserController.deleteUser);
 api.get('/users', mdAuth.ensureAuth, UserController.getUsers);
+//follows
+api.post('/users/:id/follow/community', mdAuth.ensureAuth, UserController.followCommunity);
+api.post('/users/:id/unfollow/community', mdAuth.ensureAuth, UserController.unfollowCommunity);
+api.post('/users/:id/follow/user', mdAuth.ensureAuth, UserController.followUser);
+api.post('/users/:id/unfollow/user', mdAuth.ensureAuth, UserController.unfollowUser);
 
 module.exports = api;
